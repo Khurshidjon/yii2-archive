78082ecc508fc628cc048d5fd7387bb9fc36c0cb
Khurshidjon <xurshidjon1607@gmail.com>
2020-09-17 18:53:56 +0500

    dd

--- backend/views/folders/create.php
+++ backend/views/folders/create.php
@@ -12,10 +12,15 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="folders-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
+    <?php if (Yii::$app->controller->action->id == 'create'): ?>
     <?= $this->render('_form', [
-        'model' => $model,
-        'folder_id' => $folder_id
+        'model' => $model
     ]) ?>
+    <?php elseif (Yii::$app->controller->action->id == 'child-create'): ?>
+        <?= $this->render('_form', [
+            'model' => $model,
+            'folder_id' => $folder_id
+        ]) ?>
+    <?php endif?>
 
 </div>
